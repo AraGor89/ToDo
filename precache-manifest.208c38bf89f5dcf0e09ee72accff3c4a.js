c7640da031df75be3b169ba69f459e77075223ca
GorAraqelyan <50023373+GorAraqelyan@users.noreply.github.com>
2020-05-16 15:03:27 +0400

    Updates

---
+++ precache-manifest.208c38bf89f5dcf0e09ee72accff3c4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c62f8b3e9119f807e735123e8569644",
+    "url": "/ToDo/index.html"
+  },
+  {
+    "revision": "5fbae566301fc04acf9b",
+    "url": "/ToDo/static/css/main.08750132.chunk.css"
+  },
+  {
+    "revision": "57bbf2f5c90d488e3126",
+    "url": "/ToDo/static/js/2.1a65f4fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ToDo/static/js/2.1a65f4fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fbae566301fc04acf9b",
+    "url": "/ToDo/static/js/main.0e7fbf5d.chunk.js"
+  },
+  {
+    "revision": "7924aee5b4de4d3a477f",
+    "url": "/ToDo/static/js/runtime-main.c4e2ddcc.js"
+  }
+]);
